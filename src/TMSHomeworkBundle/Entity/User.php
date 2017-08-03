253904d42b4d087c53404e9fdf4319eeae6296e1
Nebojsa Manojlovic <meddved@gmail.com>
2017-08-03 16:48:26 +0200

    Initial database and migrations setup.

---
+++ src/TMSHomeworkBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nebojsam
+ * Date: 03/08/17
+ * Time: 16:21
+ */
+
+namespace TMSHomeworkBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
